7dd4a141c493f4a809d6af1b4d238b5110ef5721
Yizun Yuan <digital@momentummedia.com.au>
2022-08-31 11:19:09 +1000

    updates

---
+++ process.php
@@ -0,0 +1,14 @@
+<?php
+
+$area = file_get_contents('area.json');
+$areas = file_get_contents('areas.json');
+
+$area = json_decode($area);
+$areas = json_decode($areas);
+
+var_dump($area->features);
+//var_dump($areas->features);
+
+$areas->features [] = $area->features;
+
+file_put_contents('areas.json', json_encode($areas));
