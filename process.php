af6fb28d670cd61e7718302f1a73d2eb471650be
Yizun Yuan <digital@momentummedia.com.au>
2022-08-31 11:31:09 +1000

    Revert "updates"
    This reverts commit c3e92962c10a5b3a90e4ad214ad0785e9ecaf2df.

--- process.php
+++ process.php
@@ -6,7 +6,7 @@ $areas = file_get_contents('areas.json');
 $area = json_decode($area);
 $areas = json_decode($areas);
 
-//var_dump($area->features);
+var_dump($area->features);
 //var_dump($areas->features);
 
 $areas->features [] = $area->features;
